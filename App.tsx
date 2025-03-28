0bf8845dd536b64b26d9836ce4c04d5827b43abd
Lucas Fernandes <lucasfernandesmarabini@gmail.com>
2025-03-28 20:25:09 -0300

    mudança de cor e adição de texto

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.texto}>
+        3ECR aula 28/03</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    fontSize: 30,
+    color: '#125',
+    fontWeight: 'bold'
+  }
 });
